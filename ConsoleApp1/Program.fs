7939b8852717e019164835c4ff4a558f924dd373
joseph skokan <joeskokan20@gmail.com>
2025-02-17 03:13:11 -0500

    notes

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -4,6 +4,7 @@ open inputs
 
 printfn "Hello from F#"
 printfn "---------------------------------"
+//we can call our greet function from strings/input and save it to a variable
 let userInfo = greet()
 printf "%s" userInfo
 
