2b298862289d40a2d1e6195dff4c0b19b62dff17
joseph skokan <joeskokan20@gmail.com>
2025-02-17 03:21:06 -0500

    multiply to core

--- ConsoleApp1/core/coref.fs
+++ ConsoleApp1/core/coref.fs
@@ -10,10 +10,17 @@ printfn "%d" (addIt 5)
 //allows for easy dynamic addition 
 printfn "Enter a Number: "
 let input = Console.ReadLine()
-
+// take the stirng input attempt to turn it into a number
+// if it is a number add 1 to it
 match Int32.TryParse(input) with
 | (true, n) -> printfn "Result: %d" (addIt n)
 | (false, _) -> printfn "Invalid number"
+
+// we can also create new function for multiplication or any operation you can think of
+// this function will multiply the input by 2
+let multiplyIt n =
+    n * 2
+printfn "%d" (multiplyIt 5)
 
 
 
