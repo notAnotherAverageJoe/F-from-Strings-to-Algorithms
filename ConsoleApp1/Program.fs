8fe1dc634ffadf7845603f03654744a1e2d7216f
joseph skokan <joeskokan20@gmail.com>
2025-02-17 03:10:50 -0500

    called greet function from seperate folder.

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,6 +1,12 @@
 ﻿open System
 open File1
+open inputs
 
 printfn "Hello from F#"
+printfn "---------------------------------"
+let userInfo = greet()
+printf "%s" userInfo
+
+
 
 
--- ConsoleApp1/strings/inputs.fs
+++ ConsoleApp1/strings/inputs.fs
@@ -1,16 +1,17 @@
 ﻿module inputs
 open System
 
-//prompt user 
-printfn "Enter your first name: "
-//get user input 
-let firstName = Console.ReadLine()
-printfn "Enter your last name: "
-let lastName = Console.ReadLine()
-printfn "Enter your age: "
-let age = Console.ReadLine()
-//place all the user information into result and print it
+let greet() =
+    //prompt user 
+    printfn "Enter your first name: "
+    //get user input 
+    let firstName = Console.ReadLine()
+    printfn "Enter your last name: "
+    let lastName = Console.ReadLine()
+    printfn "Enter your age: "
+    let age = Console.ReadLine()
+    //print all the user information into result and print it
 
-let result = sprintf "Hello %s %s, you are %s years old" firstName lastName age
+    sprintf "Hello %s %s, you are %s years old" firstName lastName age
 
 
